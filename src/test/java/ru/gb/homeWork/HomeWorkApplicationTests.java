188d6ea9b49607e49b64c33fc41aca70d8117993
Rockerok <Rockerok@mail.ru>
2022-06-19 15:51:28 +0300

    Initial commit

---
+++ src/test/java/ru/gb/homeWork/HomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.homeWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
